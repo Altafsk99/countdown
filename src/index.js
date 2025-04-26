99e6f75fbd9d4b04b91223e875e207d94fe1e10c
Altafsk99 <altafsk121999@gmail.com>
2025-04-26 13:33:12 +0400

    name update to publish

---
+++ src/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.countDown = void 0;
+const countDown = (targetDate) => {
+    const parsedDate = new Date(targetDate);
+    // Check if targetDate is a valid Date
+    if (isNaN(parsedDate.getTime())) {
+        throw new Error("Invalid targetDate format. Please provide a valid date-time string.");
+    }
+    const currentDate = new Date();
+    const diffMilliseconds = parsedDate.getTime() - currentDate.getTime();
+    if (diffMilliseconds <= 0) {
+        return {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        };
+    }
+    const remainingDays = Math.floor(diffMilliseconds / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor((diffMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const remainingMinutes = Math.floor((diffMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
+    const remainingSeconds = Math.floor((diffMilliseconds % (1000 * 60)) / 1000);
+    return {
+        days: remainingDays,
+        hours: remainingHours,
+        minutes: remainingMinutes,
+        seconds: remainingSeconds,
+    };
+};
+exports.countDown = countDown;
